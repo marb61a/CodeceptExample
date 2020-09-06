86a0ce4161b47eb893d5c791e75dbdd1b3b8de3b
Martin <marb61a@gmail.com>
2020-09-06 11:52:35 +0100

    Updated login test

--- login_test.js
+++ login_test.js
@@ -1,5 +1,27 @@
+// To run tests in codecept use the following command at the cli
+// npx codeceptjs run --steps
+
 Feature('login');
 
-Scenario('test something', (I) => {
+Scenario('test the demo site', (I) => {
+    I.amOnPage('/Login.html');
 
+    // Fill in the passsword and username text fields
+    I.fillField("UserName", "admin");
+    I.fillField("Password", "password");
+    I.click("//input[@type='submit']");
+
+    // Wait for the element to be present
+    I.waitForElement({xpath: "//input[@name='Initial']"});
+
+    // Create and save screenshot
+    // screenshot will be created and saved in output on failure
+    I.saveScreenshot("loggedin.jpg");
+
+    // Checking a radio button/check box
+    I.checkOption("Hindi");
+
+    // Checking the page title
+    I.seeTitleEquals("Execute Automation");
+    
 });
